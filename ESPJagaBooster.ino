9c2138d88f63b3365e2a2c1ac089e4d297a7e751
BeaverUI <75741310+BeaverUI@users.noreply.github.com>
2022-09-05 11:02:23 +0200

    keep WiFI+OTA updates active in standalone mode

--- ESPJagaBooster.ino
+++ ESPJagaBooster.ino
@@ -73,7 +73,7 @@
 
 // Node
 #define NODE_NAME "radiator-wk-voor" // name of this module - only use small letters and hyphens ('-')
-#define STANDALONE_MODE 0 // if set to 1, the board runs without WiFi and MQTT
+#define STANDALONE_MODE 0 // if set to 1, the board runs without MQTT. Note: WiFi and over-the-air updates are still active.
 #define SLAVE_MODE 0 // if set to 1, use control input from other node (only works if STANDALONE_MODE=0), module does not need temperature sensors in this mode
 #define MASTER_NODE_NAME "radiator-wk-voor" // when in slave mode, this is the node name of the master
 
@@ -167,63 +167,62 @@ int mqtt_reconnects = 0;
 void setup() {
   Serial.begin(115200);
 
+  // Start networking
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  // optional: fixed IP address, it is recommended to assign a fixed IP via the DHCP server instead
+  // IPAddress ip(192,168,1,31); IPAddress gateway(192,168,1,1); IPAddress subnet(255,255,0,0); WiFi.config(ip, gateway, subnet);
+  Serial.print("Attempting to connect to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  } Serial.println("");
+  Serial.println("Connected");
+  Serial.println("IP address: " + IPAddressString(WiFi.localIP()));
+
+
+  // Init OTA updates
+  ArduinoOTA.setPort(8266);    // Port defaults to 8266
+  ArduinoOTA.setHostname(NODE_NAME);  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setPassword("admin");
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  
   if(STANDALONE_MODE==0){
-    // Start networking
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    // optional: fixed IP address, it is recommended to assign a fixed IP via the DHCP server instead
-    // IPAddress ip(192,168,1,31); IPAddress gateway(192,168,1,1); IPAddress subnet(255,255,0,0); WiFi.config(ip, gateway, subnet);
-    Serial.print("Attempting to connect to WiFi...");
-    while (WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
-      delay(1000);
-    } Serial.println("");
-    Serial.println("Connected");
-    Serial.println("IP address: " + IPAddressString(WiFi.localIP()));
-  
-  
-    // Init OTA updates
-    ArduinoOTA.setPort(8266);    // Port defaults to 8266
-    ArduinoOTA.setHostname(NODE_NAME);  // Hostname defaults to esp8266-[ChipID]
-    // ArduinoOTA.setPassword("admin");
-    // Password can be set with it's md5 value as well
-    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-  
-    ArduinoOTA.onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH) {
-        type = "sketch";
-      } else { // U_FS
-        type = "filesystem";
-      }
-  
-      // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-      Serial.println("Start updating " + type);
-    });
-    ArduinoOTA.onEnd([]() {
-      Serial.println("\nEnd");
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) {
-        Serial.println("Auth Failed");
-      } else if (error == OTA_BEGIN_ERROR) {
-        Serial.println("Begin Failed");
-      } else if (error == OTA_CONNECT_ERROR) {
-        Serial.println("Connect Failed");
-      } else if (error == OTA_RECEIVE_ERROR) {
-        Serial.println("Receive Failed");
-      } else if (error == OTA_END_ERROR) {
-        Serial.println("End Failed");
-      }
-    });
-    ArduinoOTA.begin();
-  
-  
     // Start MQTT
     mqttclient.begin(MQTTSERVER, wificlient_mqtt);
     mqttclient.onMessage(handleMQTTreceive);
@@ -249,13 +248,14 @@ void setup() {
 // === Main stuff ====
 void loop() {
 
-  if(STANDALONE_MODE==0){
-    ArduinoOTA.handle();
-    handleMQTT();
-  }
-  
+  ArduinoOTA.handle();
+
   handleTemperature();
   handleControl();
+
+  if(STANDALONE_MODE==0){
+    handleMQTT();
+  }
 }
 
 // handle temperature
